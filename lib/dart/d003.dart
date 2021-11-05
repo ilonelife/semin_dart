fec7ad2c891a84912c80bd45282c4005db9e9e0e
iloneslife <jasony1@naver.com>
2021-11-05 22:01:22 +0900

    테스트3

--- lib/dart/d003.dart
+++ lib/dart/d003.dart
@@ -4,73 +4,59 @@ void main() {
   // 타입 변수명 = 값
   // int i = 10;
   // Cleric타입 cleric이름으로 = 클래릭을 만들자
-  Cleric cleric = Cleric(name: '이일원', hp: 50, mp: 10);
-  final cleric2 = Cleric(name: '홍길동');
+  Cleric cleric = Cleric(name: '이름', hp: 100, mp: 50);
+  final cleric2 = Cleric(name: '이름');
   final cleric3 = Cleric();
 
-  print('''
-  성직자 이름은: ${cleric.name}
-  핼스: ${cleric.hp}
-  마법: ${cleric.mp}
-  ----------------''');
+  // 상수 const, final
+  // final 런타임 상수 : 실행 중에 결정되는 메모리
+  // const 컴파일 상수 : 컴파일 중에 결정되는 메모리
+  const int count = 100;
 
-  cleric.selfAid();
-  print('''
-  
-  셀프 에이드 마법 사용으로 마법 ${cleric.mp} 으로 줄어 듬.
-  ''');
+  int sum = 0;
+  for (int i = 0; i < count; i++) {
+    cleric.mp = 5;
+    print('회복량: ${cleric.pray(1)}, 현재 mp: ${cleric.mp}'); // 1~3
+    sum += cleric.pray(1);
+  }
+  print('회복량 평균 : ${sum / count}');
 
-  print('''
-  3초 기도 후 마법이 ${cleric.pray(3)} 증가
-  ''');
-
-  // final 은 런타임 상수, 실행 후 날아감
-  // const 는 컴파일 상수 , 실행 후에도 남아 있음
-  print('''
-  현재 핼스: ${cleric.hp}
-  현재 마법: ${cleric.mp}''');
-
-  // print(cleric.pray(1));
-  // print('회복량 ${cleric.pray(1)}');
+  print(maxMp);
 }
 
+//// Top level ------------>
+
+const int maxHp = 50;   // 4byte
+const int maxMp = 10;   // 4byte
+
+//// Top level <------------
+
 class Cleric {
+  //// Field, 전역, Global, Property ------------>
+  // 전역 변수, Field, member 변수, Global 변수,
+
+  // public
   String name;
   int hp;
   int mp;
 
-  final int maxHp = 50; // 최대 HP
-  final int maxMp = 10;
+  // private
+  final Random _random = Random();
 
-  Cleric({this.name, this.hp, this.mp})
+  //// Field <------------
 
+  Cleric({this.name, this.hp, this.mp});
 
   void selfAid() {
-    mp -= 5;
+    mp = mp - 5 + _random.nextInt(5);
     hp = maxHp;
   }
 
-  /* 회복량은 기도한 시간(초)에 랜덤하게 0 ~ 2포인트의 보정을 한 양이다 (3초 기도하면 회복량은 3 ~ 5 포인트).
-  단, 최대 MP 보다 더 회복하는 것은 불가능 하다.
-   */
   int pray(int sec) {
-    int prayMp = 0;
+    int curMp = mp;
+    mp = min(sec + Random().nextInt(3) + mp, maxMp);
 
-    prayMp = min((sec + Random().nextInt(3)), maxMp);   // max 는 반대 함수
-
-    mp = mp + prayMp;
-
-    // mp = mp + sec;
-    // mp + _random(3);
-
-    // if ( mp > mpMax) {
-    //   mp = mpMax;
-    // }
-    return mp;
+    // 실제로 회복된 mp 양 리턴
+    return mp - curMp;
   }
-
-  String introduce() {
-    return '$hp, $mp';
-  }
-
 }
--- lib/dart/d008.dart
+++ lib/dart/d008.dart
@@ -10,50 +10,36 @@ void main() {
   print(word.isVowel(6)); // true
   print(word.isVowel(7)); // true
   print(word.isVowel(8)); // true
-
-  print('test..git... push');
-
 }
 
 class Word {
   String letters;
-  String textstring = 'a,i,u,e,o';
-
-  //List result = textstring.split(',');
-
   static const vowel = ['a', 'i', 'u', 'e', 'o'];
 
   Word(this.letters);
 
   // i번째 글자가 모음이면 true // a, i, u, e, o
   bool isVowel(int i) {
-    bool result = false;
-
-    String oneword = letters.substring(i, i + 1);
-    if (oneword == 'a' ||
-        oneword == 'i' ||
-        oneword == 'u' ||
-        oneword == 'e' ||
-        oneword == 'o') {
-      return true;
-    }
-
-    return vowel.contains(letters[i]);
-
-    return false;
-    }
-    // for (int i = 0; i < vowel.length; i++) {
-    // if (oneword == vowel[i]) {
-    // return true;
-    // } else {
-    // return false;
-    // }
-    // i번째 글자가 자음이면 true
-    bool isConsonant(int i) {
-      if(isVowel(i) == true) {
-        return false;
-      }
-      return true;
-
+    return 'aiueoAIUEO'.contains(letters.substring(i, i + 1));
   }
+
+  // i번째 글자가 자음이면 true
+  bool isConsonant(int i) {
+    return !isVowel(i);
+  }
+
+  // bool isVowel(int i) {
+  //   bool result = false;
+  //
+  //   String oneword = letters.substring(i, i + 1);
+  //   if (oneword == 'a' ||
+  //       oneword == 'i' ||
+  //       oneword == 'u' ||
+  //       oneword == 'e' ||
+  //       oneword == 'o') {
+  //     return true;
+  //   }
+  //   return result;
+  // }
+
 }
