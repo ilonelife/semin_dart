f352b6356ccc8e14abbaa062863dfea340dc101c
iloneslife <jasony1@naver.com>
2021-11-05 17:43:51 +0900

    테스트

--- lib/dart/d008.dart
+++ lib/dart/d008.dart
@@ -1,10 +1,6 @@
 void main() {
   Word word = Word('aeiueoflutter');
 
-  // List<String> wordlist = word;
-  // for (int i = 0; i < word.; i++) {
-  //
-  // }
   print(word.isVowel(0)); // true
   print(word.isVowel(1)); // false
   print(word.isVowel(2)); // false
@@ -14,31 +10,48 @@ void main() {
   print(word.isVowel(6)); // true
   print(word.isVowel(7)); // true
   print(word.isVowel(8)); // true
+
 }
 
 class Word {
   String letters;
-  String oneword;
+  String textstring = 'a,i,u,e,o';
 
-//  List<String> oneword = List<String>(5);
+  //List result = textstring.split(',');
+
+  static const vowel = ['a', 'i', 'u', 'e', 'o'];
+
   Word(this.letters);
 
   // i번째 글자가 모음이면 true // a, i, u, e, o
   bool isVowel(int i) {
-    oneword = letters.substring(i, i + 1);
+    bool result = false;
+
+    String oneword = letters.substring(i, i + 1);
     if (oneword == 'a' ||
         oneword == 'i' ||
         oneword == 'u' ||
         oneword == 'e' ||
         oneword == 'o') {
       return true;
-    } else {
-      return false;
     }
-  }
 
-  // i번째 글자가 자음이면 true
-  bool isConsonant(int i) {
+    return vowel.contains(letters[i]);
+
     return false;
+    }
+    // for (int i = 0; i < vowel.length; i++) {
+    // if (oneword == vowel[i]) {
+    // return true;
+    // } else {
+    // return false;
+    // }
+    // i번째 글자가 자음이면 true
+    bool isConsonant(int i) {
+      if(isVowel(i) == true) {
+        return false;
+      }
+      return true;
+
   }
 }
